3afdbe2eefbec28d583ccca134085ba3ff33ea86
Philip Quirke <PhilipQuirke@users.noreply.github.com>
2024-02-21 11:46:27 +1300

    Update and rename filter.py to quanta_filter.py

--- QuantaTools/quanta_filter.py
+++ QuantaTools/quanta_filter.py
@@ -1,6 +1,6 @@
 from enum import Enum
 
-class Filter(Enum):
+class QuantaFilter(Enum):
     MUST = 1  # The specified quanta value MUST exist in the object
     NOT = 2  # The specified quanta value must NOT exist in the object
     CONTAINS = 3 # The specified quanta value can equal the object value or a subset of the object value
