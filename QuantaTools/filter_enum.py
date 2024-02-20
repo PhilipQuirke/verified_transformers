c8c82f25e9db996fe59adb5c8a61a6df139bfbb2
Philip Quirke <PhilipQuirke@users.noreply.github.com>
2024-02-20 15:34:37 +1300

    Add files via upload

---
+++ QuantaTools/filter_enum.py
@@ -0,0 +1,15 @@
+import sys
+from enum import Enum
+
+class QuantaFilter(Enum):
+    MUST = 1  # The specified quanta value MUST exist in the object
+    NOT = 2  # The specified quanta value must NOT exist in the object
+    CONTAINS = 3 # The specified quanta value can equal the object value or a subset of the object value
+    MAY = 4  # The specified quanta value MAY exist (Indefinite. Used in a hypothesis)
+
+    
+    def __eq__(self, other):
+        """Necessary because of extremely frustrating error that arises with load_ext autoreload (because this uses importlib under the hood: https://stackoverflow.com/questions/66458864/enum-comparison-become-false-after-reloading-module)"""
+
+        assert isinstance(other, QuantaFilter)
+        return self.value == other.value
