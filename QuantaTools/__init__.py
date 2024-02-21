606ec09865f0c02e59bf44dd34c3b22826a61d5f
Philip Quirke <PhilipQuirke@users.noreply.github.com>
2024-02-21 13:00:06 +1300

    Update __init__.py

--- QuantaTools/__init__.py
+++ QuantaTools/__init__.py
@@ -1,3 +1,5 @@
-from .quanta_filter import QuantaFilter
 from .useful_node import UsefulNode
 from .useful_info import UsefulInfo, useful_info
+
+from .quanta_filter import QuantaFilter
+from .quanta_map import calc_quanta_map
