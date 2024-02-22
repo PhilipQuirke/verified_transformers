3565d2ad687c50ad70af99f7bb0a758768ec36a2
Philip Quirke <PhilipQuirke@users.noreply.github.com>
2024-02-23 09:46:36 +1300

    Update __init__.py

--- QuantaTools/__init__.py
+++ QuantaTools/__init__.py
@@ -1,4 +1,4 @@
-from .useful_node import row_location_name, location_name, NodeLocation, UsefulNode
+from .useful_node import position_name, position_name_to_int, row_location_name, location_name, NodeLocation, UsefulNode  
 from .useful_info import UsefulInfo, useful_info
 
 from .quanta_filter import QuantaFilter
