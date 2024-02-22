723a3238a150dbe2785ed1a9fa6a091bdf1c2654
Philip Quirke <PhilipQuirke@users.noreply.github.com>
2024-02-23 11:49:25 +1300

    Update __init__.py

--- QuantaTools/__init__.py
+++ QuantaTools/__init__.py
@@ -1,6 +1,7 @@
+from .quanta_filter import QuantaFilter
+from .quanta_type import QuantaType
+
 from .useful_node import position_name, position_name_to_int, row_location_name, location_name, NodeLocation, UsefulNode  
 from .useful_info import UsefulInfo, useful_info
 
-from .quanta_filter import QuantaFilter
-from .quanta_type import QuantaType
 from .quanta_map import create_custom_colormap, calc_quanta_map
