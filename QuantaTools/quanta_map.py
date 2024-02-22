8dbdb4f5825ea9272baf9a751f884f594fa2af01
Philip Quirke <PhilipQuirke@users.noreply.github.com>
2024-02-23 09:47:50 +1300

    Update quanta_map.py

--- QuantaTools/quanta_map.py
+++ QuantaTools/quanta_map.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
 import textwrap
 
-from .useful_node import NodeLocation, UsefulNode, row_location_name
+from .useful_node import position_name, position_name_to_int, row_location_name, location_name, NodeLocation, UsefulNode  
 from .useful_info import UsefulInfo, useful_info
 
 
