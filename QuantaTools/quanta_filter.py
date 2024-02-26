470fdf7d2b38bd863bf28cb84f829052254e1218
Philip Quirke <PhilipQuirke@users.noreply.github.com>
2024-02-27 10:51:28 +1300

    Update quanta_filter.py

--- QuantaTools/quanta_filter.py
+++ QuantaTools/quanta_filter.py
@@ -6,3 +6,17 @@ class QuantaFilter(Enum):
     CONTAINS = 3 # The specified quanta value can equal the object value or a subset of the object value
     MAY = 4  # The specified quanta value MAY exist (Indefinite. Used in a hypothesis)
     MUST_BY = 5  # The specified quanta value MUST exist in the object at or before the specified position
+
+
+def quanta_filter_to_str( the_filter ):
+    if the_filter == QuantaFilter.MUST:
+        return "must"
+    if the_filter == QuantaFilter.NOT:
+        return "not"
+    if the_filter == QuantaFilter.CONTAINS:
+        return "contains"
+    if the_filter == QuantaFilter.MAY:
+        return "may"
+    if the_filter == QuantaFilter.MUST_BY:
+        return "must-by"
+        
