c7a68851e8400966283dec40c9d23cae93e8ab99
Philip Quirke <PhilipQuirke@users.noreply.github.com>
2024-02-27 11:58:07 +1300

    Update filter_node.py

--- QuantaTools/filter_node.py
+++ QuantaTools/filter_node.py
@@ -1,3 +1,10 @@
+from .quanta_filter import QuantaFilter
+from .quanta_type import QuantaType 
+
+from .useful_node import position_name, position_name_to_int, row_location_name, location_name, NodeLocation, UsefulNode  
+from .useful_info import UsefulInfo, useful_info
+
+
 class FilterNode:
     def evaluate(self, useful_node):
         pass
