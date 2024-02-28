fdeff203694493539bff005fb8246a1c9e17310c
Philip Quirke <PhilipQuirke@users.noreply.github.com>
2024-02-28 14:11:45 +1300

    Update quanta_map_failperc.py

--- QuantaTools/quanta_map_failperc.py
+++ QuantaTools/quanta_map_failperc.py
@@ -8,7 +8,6 @@ from .useful_info import UsefulInfo, useful_info
 # If the node fails on say 3 of 1000 questions, rather than return 0%, for clarity we show <1%
 def get_quanta_fail_perc( node, major_tag, minor_tag, shades):
   cell_text = node.only_tag( major_tag )
-
   value = int(cell_text) if cell_text != "" else 0
 
   color_index = value // shades
