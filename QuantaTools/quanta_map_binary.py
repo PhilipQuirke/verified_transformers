baf2d5776bcd10cedf22b6427efb4db9ffbef71d
Philip Quirke <PhilipQuirke@users.noreply.github.com>
2024-02-28 14:04:32 +1300

    Create quanta_map_binary.py

---
+++ QuantaTools/quanta_map_binary.py
@@ -0,0 +1,21 @@
+from .quanta_filter import QuantaFilter
+from .quanta_type import QuantaType
+from .useful_node import position_name, position_name_to_int, row_location_name, location_name, NodeLocation, UsefulNode 
+from .useful_info import UsefulInfo, useful_info
+
+
+def get_binary_category(node, major_tag, minor_tag, shades):
+
+  cell_text = ""
+  color_index = 0
+
+  node_tags = node.filter_tags( major_tag, minor_tag )
+  for tag in node_tags:
+    cell_text += tag + " "
+
+  if cell_text == "":
+    cell_text = "??"
+  else:
+    color_index = 1
+
+  return cell_text, color_index
