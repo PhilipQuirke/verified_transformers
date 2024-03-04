d6e308e2d030ea23a9e0a550beb69abfa3b4bd46
Philip Quirke <PhilipQuirke@users.noreply.github.com>
2024-03-04 13:41:14 +1300

    Create token_to_char.py

---
+++ QuantaTools/token_to_char.py
@@ -0,0 +1,16 @@
+from .useful_info import UsefulInfo, useful_info
+
+
+# Map from token to character
+def token_to_char(i):
+  for char, token in useful_info.char_to_token.items():
+    if token == i:
+      return char
+
+  # Should never happen
+  assert False
+
+
+# Map from tokens to string
+def tokens_to_string(tokens):
+    return "".join([token_to_char(i) for i in tokens])
