9ed22502e60e42f4f94e637dbc80472acf79011f
Philip Quirke <PhilipQuirke@users.noreply.github.com>
2024-03-13 10:46:06 +1300

    Update __init__.py

--- QuantaTools/__init__.py
+++ QuantaTools/__init__.py
@@ -1,5 +1,5 @@
 from .quanta_filter import QuantaFilter
-from .quanta_type import QuantaType 
+from .quanta_type import QuantaType, MAX_ATTENTION_TAGS, MIN_ATTENTION_PERC 
 
 from .useful_node import position_name, position_name_to_int, row_location_name, location_name, NodeLocation, str_to_node_location, UsefulNode  
 from .useful_info import answer_name, UsefulInfo, useful_info
@@ -7,7 +7,7 @@ from .useful_info import answer_name, UsefulInfo, useful_info
 from .token_to_char import token_to_char, tokens_to_string 
 
 from .quanta_map import create_custom_colormap, calc_quanta_map
-from .quanta_map_attention import MAX_ATTENTION_TAGS, MIN_ATTENTION_PERC, get_quanta_attention
+from .quanta_map_attention import get_quanta_attention
 from .quanta_map_failperc import get_quanta_fail_perc
 from .quanta_map_binary import get_quanta_binary
 from .quanta_map_impact import get_answer_impact, get_question_answer_impact, is_answer_sequential, compact_answer_if_sequential, get_quanta_impact
