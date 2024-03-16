6efb7856d237ddc1c68c45ec1884c881c0d3e83f
Philip Quirke <PhilipQuirke@users.noreply.github.com>
2024-03-16 13:29:08 +1300

    Update maths_vocab.py

--- QuantaTools/maths_vocab.py
+++ QuantaTools/maths_vocab.py
@@ -1,4 +1,5 @@
 from .useful_info import useful_info
+from .useful_token_to_char import token_to_char, tokens_to_string
 
 
 class MathsTokens:
@@ -19,3 +20,8 @@ def set_maths_vocabulary():
   useful_info.char_to_token['='] = EQUALS_INDEX
   useful_info.char_to_token['*'] = MULT_INDEX
   useful_info.char_to_token['\\'] = DIV_INDEX
+
+  # Unit tests
+  assert token_to_char(4) == '4'
+  assert token_to_char(MULT_INDEX) == '*'
+  assert tokens_to_string([EQUALS_INDEX,4,0,7]) == '=407'
