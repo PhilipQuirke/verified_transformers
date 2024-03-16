d328b48e8737f971725387cde0f8c92765590fec
Philip Quirke <PhilipQuirke@users.noreply.github.com>
2024-03-16 16:56:35 +1300

    Update quanta_map_failperc.py

--- QuantaTools/quanta_map_failperc.py
+++ QuantaTools/quanta_map_failperc.py
@@ -5,7 +5,7 @@ from .useful_node import NodeLocation, UsefulNode
 
 # Return the percentage of questions that the model failed to predict when this node was ablated.
 # If the node fails on say 3 of 1000 questions, rather than return 0%, for clarity we show <1%
-def get_quanta_fail_perc( node, major_tag, minor_tag, shades):
+def get_quanta_fail_perc( useful_info, node, major_tag, minor_tag, shades):
   cell_text = node.only_tag( major_tag )
   value = int(cell_text) if cell_text != "" else 0
 
