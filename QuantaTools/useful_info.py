05d8a27b0abcc4751a6b0f2cea3a9c5138aa276f
Philip Quirke <PhilipQuirke@users.noreply.github.com>
2024-03-16 16:44:09 +1300

    Update useful_info.py

--- QuantaTools/useful_info.py
+++ QuantaTools/useful_info.py
@@ -2,53 +2,22 @@ import matplotlib.pyplot as plt
 
 from .quanta_filter import QuantaFilter
 from .quanta_type import QuantaType
+from .quanta_config import QuantaConfig
+
 from .useful_node import position_name, position_name_to_int, row_location_name, location_name, answer_name, NodeLocation, UsefulNode, UsefulNodeList
 
 
-class UsefulInfo(UsefulNodeList):
-  
-  # Vocabulary: Map from each character to each token
-  char_to_token : dict 
-
-  # The number of "question" (input) token positions e.g. len("12340+12340=")
-  num_question_positions : int 
-  
-  # The number of "answer" (output) token positions  e.g. len("+024680") 
-  num_answer_positions : int
-  
-  # Do we name the answer tokens as A5, A4, A3, A2, A1, A0 or A0, A1, A2, A3, A4, A5?
-  answer_meanings_ascend : bool  
-  
-  # List of (short) strings representing the meaning of each token position.
-  # For example D5, D4, D3, D2, D1, D0, +, D'5, D'4, D'3, D'2, D'1, D'0, =, A6, A5, A4, A3, A2, A1, A0
-  # Used in node tag, in the column headings of quanta-maps, etc. 
-  token_position_meanings = []
-
-  # sparce ordered list of useful (question and answer) token positions e.g. 0,1,8,9,10,11
-  positions = []
-
-
-  # Default list of strings representing the token positions meanings
-  def default_token_position_meanings(self):
-    self.token_position_meanings = []
-    for i in range(self.num_question_positions):
-      self.token_position_meanings += ["P"+str(i)]
-    for i in range(self.num_answer_positions):
-      self.token_position_meanings += [answer_name(i if self.answer_meanings_ascend else self.num_answer_positions - i - 1 )]
-
-  
-  def initialize_token_positions(self, num_question_positions, num_answer_positions, answer_meanings_ascend ):
-    self.num_question_positions = num_question_positions
-    self.num_answer_positions = num_answer_positions
-    self.answer_meanings_ascend = answer_meanings_ascend   
-    self.default_token_position_meanings()
-
-  
+class UsefulInfo(QuantaConfig):
+ 
   def __init__(self):
     super().__init__()
-    self.char_to_token = {}
+
+    # sparce ordered list of useful (question and answer) token positions e.g. 0,1,8,9,10,11
     self.positions = []
-    self.initialize_token_positions(12, 7, True)
+
+    # List of the useful attention heads and MLP neurons in the model 
+    self.nodes = UsefulNodeList()
+
  
   
   def min_useful_position(self):
