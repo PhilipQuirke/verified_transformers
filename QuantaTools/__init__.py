508c2d9d6171b3d5e33e7a7aaa6e0d796fb802c8
Philip Quirke <PhilipQuirke@users.noreply.github.com>
2024-03-16 13:26:47 +1300

    Update __init__.py

--- QuantaTools/__init__.py
+++ QuantaTools/__init__.py
@@ -4,11 +4,12 @@ from .quanta_filter_node import FilterNode, FilterAnd, FilterOr, FilterHead, Fil
 
 from .useful_node import position_name, position_name_to_int, row_location_name, location_name, answer_name, NodeLocation, str_to_node_location, UsefulNode, UsefulNodeList
 from .useful_info import UsefulInfo, useful_info
-
-from .token_to_char import token_to_char, tokens_to_string 
+from .useful_token_to_char import token_to_char, tokens_to_string 
 
 from .quanta_map import create_custom_colormap, calc_quanta_map
 from .quanta_map_attention import get_quanta_attention
 from .quanta_map_failperc import get_quanta_fail_perc
 from .quanta_map_binary import get_quanta_binary
 from .quanta_map_impact import get_answer_impact, get_question_answer_impact, is_answer_sequential, compact_answer_if_sequential, get_quanta_impact
+
+from .maths_vocab import MathTokens, set_maths_vocabulary
