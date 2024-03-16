c58f5f1b5915059816c5f229304bb88e1d6bd4a5
Philip Quirke <PhilipQuirke@users.noreply.github.com>
2024-03-16 16:46:59 +1300

    Update quanta_map_binary.py

--- QuantaTools/quanta_map_binary.py
+++ QuantaTools/quanta_map_binary.py
@@ -1,7 +1,6 @@
 from .quanta_filter import QuantaFilter
 from .quanta_type import QuantaType
-from .useful_node import position_name, position_name_to_int, row_location_name, location_name, NodeLocation, UsefulNode 
-from .useful_info import UsefulInfo, useful_info
+from .useful_node import NodeLocation, UsefulNode 
 
 
 # Either the node has or doesnt have matching tag(s). Show the tag(s) or show "??"
