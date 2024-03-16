f2711e26869578a40827455a9bd920299e1554de
Philip Quirke <PhilipQuirke@users.noreply.github.com>
2024-03-16 13:09:16 +1300

    Update maths_vocab.py

--- QuantaMaths/maths_vocab.py
+++ QuantaMaths/maths_vocab.py
@@ -1,5 +1,14 @@
 from ..\QuantaTools\useful_info import useful_info
 
+class MathsTokens:
+  # Tokens used in arithmetic vocab. (Token indexes 0 to 9 represent digits 0 to 9)
+  PLUS_INDEX = 10
+  MINUS_INDEX = 11
+  EQUALS_INDEX = 12
+  MULT_INDEX = 13
+  DIV_INDEX = 14
+  MAX_INDEX = DIV_INDEX
+
 
 # Vocabulary dictionary: Mapping from character (key) to token (value)
 def set_maths_vocabulary():
