343f29a4a72043a1ceb988a880cb49b033bfb187
Philip Quirke <PhilipQuirke@users.noreply.github.com>
2024-03-16 16:46:28 +1300

    Update quanta_map_failperc.py

--- QuantaTools/quanta_map_failperc.py
+++ QuantaTools/quanta_map_failperc.py
@@ -1,7 +1,6 @@
 from .quanta_filter import QuantaFilter
 from .quanta_type import QuantaType
-from .useful_node import position_name, position_name_to_int, row_location_name, location_name, NodeLocation, UsefulNode 
-from .useful_info import UsefulInfo, useful_info
+from .useful_node import NodeLocation, UsefulNode 
 
 
 # Return the percentage of questions that the model failed to predict when this node was ablated.
