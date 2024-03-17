442a40776d645f6d00d015ef045afe7b6b675788
Philip Quirke <philipquirkenz@gmail.com>
2024-03-18 11:21:08 +1300

    rename tags

--- QuantaTools/maths_complexity.py
+++ QuantaTools/maths_complexity.py
@@ -4,7 +4,7 @@ import transformer_lens.utils as utils
 from .quanta_type import QuantaType
 from .maths_utilities import int_to_answer_str, tokens_to_unsigned_int, tokens_to_answer
 from .maths_vocab import MathsTokens
-from .maths_tag import MathsTag
+from .maths_tag import MathsBehavior
 
 
 # Analyse and return the question complexity for the Addition (S0 to S4) or Subtraction (M0 to NG) questions
@@ -25,35 +25,35 @@ def get_maths_question_complexity(cfg, question):
         mc[cfg.n_digits-1-dn] = 1
 
     if torch.sum(mc) == 0:
-      return QuantaType.MATH_ADD, MathsTag.ADD_S0_TAG
+      return QuantaType.MATH_ADD, MathsBehavior.ADD_S0_TAG
 
     if torch.sum(ms) == 0:
-      return QuantaType.MATH_ADD, MathsTag.ADD_S1_TAG
+      return QuantaType.MATH_ADD, MathsBehavior.ADD_S1_TAG
 
     for dn in range(cfg.n_digits-4):
       if mc[dn] == 1 and ms[dn+1] == 1 and ms[dn+2] == 1 and ms[dn+3] == 1 and ms[dn+4] == 1:
-        return QuantaType.MATH_ADD, MathsTag.ADD_S5_TAG # MC cascades 4 or more digits
+        return QuantaType.MATH_ADD, MathsBehavior.ADD_S5_TAG # MC cascades 4 or more digits
 
     for dn in range(cfg.n_digits-3):
       if mc[dn] == 1 and ms[dn+1] == 1 and ms[dn+2] == 1 and ms[dn+3] == 1:
-        return QuantaType.MATH_ADD, MathsTag.ADD_S4_TAG # MC cascades 3 or more digits
+        return QuantaType.MATH_ADD, MathsBehavior.ADD_S4_TAG # MC cascades 3 or more digits
 
     for dn in range(cfg.n_digits-2):
       if mc[dn] == 1 and ms[dn+1] == 1 and ms[dn+2] == 1:
-        return QuantaType.MATH_ADD, MathsTag.ADD_S3_TAG # MC cascades 2 or more digits
+        return QuantaType.MATH_ADD, MathsBehavior.ADD_S3_TAG # MC cascades 2 or more digits
 
     for dn in range(cfg.n_digits-1):
       if mc[dn] == 1 and ms[dn+1] == 1:
-        return QuantaType.MATH_ADD, MathsTag.ADD_S2_TAG # Simple US 9
+        return QuantaType.MATH_ADD, MathsBehavior.ADD_S2_TAG # Simple US 9
 
-    return QuantaType.MATH_ADD, MathsTag.ADD_S1_TAG
+    return QuantaType.MATH_ADD, MathsBehavior.ADD_S1_TAG
 
 
   if operator == MathsTokens.MINUS:
     a = tokens_to_unsigned_int( question, 0, cfg.n_digits )
     b = tokens_to_unsigned_int( question, cfg.n_digits + 1, cfg.n_digits )
     if a - b < 0:
-      return QuantaType.MATH_SUB, MathsTag.SUB_NG_TAG
+      return QuantaType.MATH_SUB, MathsBehavior.SUB_NG_TAG
 
     # Locate the BO and MZ digits (if any)
     bo = torch.zeros(cfg.n_digits).to(torch.int64)
@@ -66,7 +66,7 @@ def get_maths_question_complexity(cfg, question):
 
     # Evaluate BaseSub questions - when no column generates a Borrow One
     if torch.sum(bo) == 0:
-      return QuantaType.MATH_SUB, MathsTag.SUB_S0_TAG
+      return QuantaType.MATH_SUB, MathsBehavior.SUB_S0_TAG
 
     # Evaluate subtraction "cascade multiple steps" questions
     for dn in range(cfg.n_digits-3):
@@ -76,16 +76,28 @@ def get_maths_question_complexity(cfg, question):
     # Evaluate subtraction "cascade multiple steps" questions
     for dn in range(cfg.n_digits-2):
       if bo[dn] == 1 and mz[dn+1] == 1 and mz[dn+2] == 1:
-        return QuantaType.MATH_SUB, MathsTag.SUB_S3_TAG # BO cascades 2 or more digits
+        return QuantaType.MATH_SUB, MathsBehavior.SUB_S3_TAG # BO cascades 2 or more digits
 
     # Evaluate subtraction "cascade 1" questions
     for dn in range(cfg.n_digits-1):
       if bo[dn] == 1 and mz[dn+1] == 1:
-        return QuantaType.MATH_SUB, MathsTag.SUB_S2_TAG # BO cascades 1 digit
+        return QuantaType.MATH_SUB, MathsBehavior.SUB_S2_TAG # BO cascades 1 digit
 
-    return QuantaType.MATH_SUB, MathsTag.SUB_S1_TAG
+    return QuantaType.MATH_SUB, MathsBehavior.SUB_S1_TAG
 
 
   # Should never get here
   print("get_question_complexity OP? exception", question)
   return "", "OP?"
+
+
+# Analyze the tags associated with node, to show the minimum mathematical complexity
+# That is, what is the simpliest type of question that this node is needed for?
+def get_maths_min_complexity(cfg, node, major_tag, minor_tag, shades):
+    color_index = 0
+    cell_text = node.min_tag_suffix( major_tag, minor_tag )
+    if cell_text != "" :
+        cell_text = cell_text[0:2]
+        color_index = int(cell_text[1]) if len(cell_text) > 1 and cell_text[1].isdigit() else shades-1
+
+    return cell_text, color_index
--- QuantaTools/maths_tag.py
+++ QuantaTools/maths_tag.py
@@ -1,6 +1,7 @@
 
 
-class MathsTag:
+# These are maths behaviors: quanta we can evaluate for each node based just on that node
+class MathsBehavior:
   # Minor "maths" tags related to major tag QuantaType.PCA:
   # PCA says the node outputs is interpretable aligned to the T8,T9,T10 questions, giving 2 or 3 distinct output clusters
   PCA_ADD_TAG = "PA"
@@ -24,16 +25,19 @@ class MathsTag:
   SUB_NG_TAG = "NG" # Hard. Subtraction question has negative answer
 
 
+# These are maths algorthmic purposes: interpretations we assign to each node, partially based on its behavior
 # Minor "maths" tags related to major tag QuantaType.ALGO:
 # A node may serve multiple purposes and so have more than 1 of these tags.
-class AlgoTag:
+class MathsAlgorithm:
   ADD_BA_TAG = "BA" # Addition - Base Add (Dn, D'n)
   ADD_MC_TAG = "MC" # Addition - Make Carry (Dn, D'n)
   ADD_US_TAG = "US" # Addition - Use Sum 9 (Dn, D'n)
   ADD_TC_TAG = "TC" # Addition - TriCase (Dn, D'n)
+  
   SUB_BS_TAG = "BS" # Subtraction - Base Sub (Dn, D'n)
   SUB_BO_TAG = "BO" # Subtraction - Borrow One (Dn, D'n)
   SUB_SZ_TAG = "SZ" # Subtraction - Sum Zero (Dn, D'n)
   SUB_NG_TAG = "NG" # Subtraction - Answer is negative (that is D < D')
+  
   MIX_OP_TAG = "OP" # Add/Sub - Attends to operation token (in the middle of the question)
   MIX_SG_TAG = "SG" # Add/Sub - Attends to answer sign (+/-) token (at the start of the answer)
