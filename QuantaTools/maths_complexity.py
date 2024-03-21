5b2e279d74d86e9213875a91cff6e6295bd32a33
Philip Quirke <philipquirkenz@gmail.com>
2024-03-21 15:29:06 +1300

    show_maths_quanta_for_position_nodes

--- QuantaTools/maths_complexity.py
+++ QuantaTools/maths_complexity.py
@@ -1,9 +1,16 @@
+import matplotlib.pyplot as plt
 import torch
 import transformer_lens.utils as utils
 
+from .useful_node import position_name, position_name_to_int, UsefulNodeList 
 from .quanta_constants import QType
+from .quanta_filter import FilterAlgo, FilterPosition, filter_nodes
 from .maths_utilities import tokens_to_unsigned_int
 from .maths_constants import MathsToken, MathsBehavior
+from .quanta_map_attention import get_quanta_attention
+from .quanta_map_failperc import get_quanta_fail_perc
+from .quanta_map_binary import get_quanta_binary
+from .quanta_map_impact import get_answer_impact, get_question_answer_impact, is_answer_sequential, compact_answer_if_sequential, get_quanta_impact, sort_unique_digits
 
 
 # Analyse and return the question complexity for the Addition (S0 to S4) or Subtraction (M0 to NG) questions
@@ -100,3 +107,36 @@ def get_maths_min_complexity(_, node, major_tag, minor_tag, num_shades):
         color_index = int(cell_text[1]) if len(cell_text) > 1 and cell_text[1].isdigit() else num_shades-1
 
     return cell_text, color_index
+
+
+
+# Show the quanta that are known for the specified position for each useful node
+def show_maths_quanta_for_position_nodes(cfg, position, node, major_tag, minor_tag, num_shades):
+
+    columns = ["Pos Meaning", "Useful Node", "Answer Impact", "Algo Purpose", "Attention", "Add Complexity", "Sub Complexity"]
+    data = None
+
+    nodelist = filter_nodes(cfg.useful_nodes, FilterPosition(position_name(position)))
+    for node in nodelist.nodes:
+        position_meaning = cfg.token_position_meanings[position]
+        node_name = node.name()
+        node_algorithm_purpose, _ = get_quanta_binary( cfg, node, QType.ALGO, "", 2)
+        node_impact, _ = get_quanta_impact( cfg, node, QType.IMPACT, "", 2 )
+        node_attention, _ = get_quanta_attention( cfg, node, QType.ATTENTION, "", 2 )
+        node_add_complexity, _ = get_maths_min_complexity( cfg, node, QType.MATH_ADD, "", 2)
+        node_sub_complexity, _ = get_maths_min_complexity( cfg, node, QType.MATH_SUB, "", 2)
+
+        if data is None:
+            data = [[position_meaning, node_name,node_impact,node_algorithm_purpose,node_attention,node_add_complexity,node_sub_complexity]]
+        else:
+            data += [[position_meaning, node_name,node_impact,node_algorithm_purpose,node_attention,node_add_complexity,node_sub_complexity]]
+
+    if not data is None:
+        _, ax = plt.subplots(figsize=(12,2))
+        ax.axis('tight')
+        ax.axis('off')
+
+        table = ax.table(cellText=data, colLabels=columns, loc='center', cellLoc='center')
+        table.auto_set_font_size(False)
+        table.set_fontsize(10)  # Set the font size here
+        table.scale(1, 1.5)  # The first parameter scales column widths, the second scales row heights
--- QuantaTools/quanta_map_binary.py
+++ QuantaTools/quanta_map_binary.py
@@ -1,18 +1,14 @@
 
 
-# Either the node has or doesnt have matching tag(s). Show the tag(s) or show "??"
+# Either the node has or doesnt have matching tag(s). Return the matching tag(s) if any
 def get_quanta_binary(_, node, major_tag, minor_tag, __):
 
     cell_text = ""
-    color_index = 0
 
     node_tags = node.filter_tags( major_tag, minor_tag )
     for tag in node_tags:
         cell_text += tag + " "
 
-    if cell_text == "":
-        cell_text = "??"
-    else:
-        color_index = 1
+    color_index = 0 if cell_text == "" else 1
 
     return cell_text, color_index
