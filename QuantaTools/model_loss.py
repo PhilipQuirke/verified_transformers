7a4164eaf6029c715b691d6f62ceaad89681f0cf
Amir Abdullah <amir@cynch.ai>
2024-04-07 06:18:12 -0600

    fix config files.

--- QuantaTools/model_loss.py
+++ QuantaTools/model_loss.py
@@ -1,6 +1,10 @@
 import torch
 import torch.nn.functional as F
+from tqdm.notebook import tqdm
 
+from .ablate_config import acfg
+from .maths_data_generator import maths_data_generator
+from .maths_test_questions import test_maths_questions_by_complexity, test_maths_questions_by_impact
 
 # Calculate the per-token probability by comparing a batch of prediction "logits" to answer "tokens"
 def logits_to_tokens_loss(cfg, logits, tokens):
@@ -11,7 +15,7 @@ def logits_to_tokens_loss(cfg, logits, tokens):
     ans_logits = logits[:, -(n_answer_digits+1):-1]
 
     # Convert raw score (logits) vector into a probability distribution.
-    # Emphasize the largest scores and suppress the smaller ones, to make them more distinguishable.
+    # Emphasizes the largest scores and suppress the smaller ones, to make them more distinguishable.
     ans_probs = F.log_softmax(ans_logits.to(torch.float64), dim=-1)
 
     max_prob_tokens = torch.argmax(ans_probs, dim=-1)
@@ -30,22 +34,20 @@ def loss_fn(ans_loss):
     return -ans_loss.mean(0)
 
 
-def one_million_questions_core():
-  global ds
-
+def one_million_questions_core(cfg):
   acfg.verbose = False
 
   cfg.analysis_seed = 345621 # Randomly chosen
-  ds = qt.maths_data_generator(cfg) # Re-initialise the data generator
+  local_ds = maths_data_generator() # Re-initialise the data generator
 
   the_successes = 0
   the_fails = 0
 
   num_batches = 1000000//cfg.batch_size
-  for epoch in tqdm.notebook.tqdm(range(num_batches)):
-      tokens = next(ds)
+  for epoch in tqdm(range(num_batches)):
+      tokens = next(local_ds)
 
-      the_fails = qt.test_maths_questions_by_impact(cfg, acfg, tokens, 0, False)
+      the_fails = test_maths_questions_by_impact(cfg, acfg, tokens, 0, False)
 
       if the_fails> 0:
         break
@@ -60,9 +62,12 @@ def one_million_questions_core():
     "WARNING: Model is not fully accurate. It failed the 1M Q test"
 
 
-def one_million_questions():
+def one_million_questions(cfg):
   store_perc_sub = cfg.perc_sub
   store_perc_mult = cfg.perc_mult
+
+  def print_config():
+      print("%Mult=", cfg.perc_mult, "%Sub=", cfg.perc_sub, "%Add=", cfg.perc_add(), "File", cfg.file_config_prefix())
 
   print_config()
   print()
@@ -71,15 +76,14 @@ def one_million_questions():
     print("Addition:")
     cfg.perc_sub = 0
     cfg.perc_mult = 0
-    one_million_questions_core()
+    one_million_questions_core(cfg)
 
   if store_perc_sub > 0:
     print("Subtraction:")
     cfg.perc_sub = 100
     cfg.perc_mult = 0
-    one_million_questions_core()
+    one_million_questions_core(cfg)
     print()
 
   cfg.perc_sub = store_perc_sub
   cfg.perc_mult = store_perc_mult
-
