eb0bbcb0e293b90fecdd28005bd654c454a2f724
Philip Quirke <philipquirkenz@gmail.com>
2024-04-11 15:15:51 +1200

    Unit test useful_node

--- Tests/tests.py
+++ Tests/tests.py
@@ -1,11 +1,10 @@
 import unittest
 
-from torch._C import _create_function_from_graph
-
-from QuantaTools.quanta_constants import QType
-
 from QuantaTools.model_token_to_char import token_to_char, tokens_to_string
 
+from QuantaTools.useful_node import UsefulNode
+
+from QuantaTools.quanta_constants import QType
 from QuantaTools.quanta_map_impact import sort_unique_digits
 
 from QuantaTools.maths_config import MathsConfig
@@ -17,8 +16,41 @@ from QuantaTools.maths_test_questions import make_maths_m0_questions_and_answers
 from QuantaTools.maths_test_questions import make_maths_n1_questions_and_answers, make_maths_n2_questions_and_answers, make_maths_n3_questions_and_answers, make_maths_n4_questions_and_answers
 
 
+class TestUseful(unittest.TestCase):
 
-class TestYourModule(unittest.TestCase):
+
+    def test_useful_node(self):
+        
+        useful_node = UsefulNode(1, 2, True, 3, [])
+
+        # Add 6 distinct tags
+        useful_node.add_tag( "Major0", "Minor1" )
+        useful_node.add_tag( "Major0", "Minor2" )
+        useful_node.add_tag( "Major1", "Minor1" )
+        useful_node.add_tag( "Major1", "Minor2" )
+        useful_node.add_tag( "Major2", "Minor1" )
+        useful_node.add_tag( "Major2", "Minor2" )
+        self.assertEqual( len(useful_node.tags), 6)
+        
+        # Add 2 duplicate tags
+        useful_node.add_tag( "Major1", "Minor1" )
+        useful_node.add_tag( "Major2", "Minor2" )
+        self.assertEqual( len(useful_node.tags), 6)
+
+        # Remove 2 tags
+        useful_node.reset_tags( "Major2" )
+        self.assertEqual( len(useful_node.tags), 4)
+
+        # Remove 1 tag
+        useful_node.reset_tags( "Major0", "Minor2" )
+        self.assertEqual( len(useful_node.tags), 3)
+
+        # Remove all tags
+        useful_node.reset_tags( "" )
+        self.assertEqual( len(useful_node.tags), 0)
+
+
+class TestMaths(unittest.TestCase):
 
 
     def test_int_to_answer_str(self):
