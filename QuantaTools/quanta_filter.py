cf4104dd7537a9a52b13a83535e45d6b9b628ad2
Philip Quirke <philipquirkenz@gmail.com>
2024-04-12 17:54:00 +1200

    tweak

--- QuantaTools/quanta_filter.py
+++ QuantaTools/quanta_filter.py
@@ -162,7 +162,6 @@ def print_algo_purpose_results(cfg):
     num_heads_with_purpose = algo_nodes.num_heads()
     num_neurons_with_purpose = algo_nodes.num_neurons()
 
-    print()
     print(f"{num_heads_with_purpose} of {num_heads} useful attention heads ({num_heads_with_purpose / num_heads * 100:.2f}%) have an algorithmic purpose assigned." )
     print(f"{num_neurons_with_purpose} of {num_neurons} useful MLP neurons ({num_neurons_with_purpose / num_neurons * 100:.2f}%) have an algorithmic purpose assigned." )
 
