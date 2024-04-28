8fc0282bb14b0dc68a2ce98670ebc02a25d64818
Amir Abdullah <amir@cynch.ai>
2024-04-28 08:41:01 -0600

    add initialization.

--- QuantaTools/maths_tools/__init__.py
+++ QuantaTools/maths_tools/__init__.py
@@ -1,1 +1,2 @@
 from .maths_pca import _build_title_and_error_message, pca_op_tag, plot_pca_for_an
+from .maths_test_questions.tricase_test_questions_generator import TOTAL_TRICASE_QUESTIONS, make_maths_tricase_questions
