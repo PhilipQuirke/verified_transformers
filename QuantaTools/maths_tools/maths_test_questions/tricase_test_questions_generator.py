8b4a8fedd6cd10c4870d46a1a9728504148dbbea
Amir Abdullah <amir@cynch.ai>
2024-05-11 17:59:42 -0600

    Add missing behaviors

--- QuantaTools/maths_tools/maths_test_questions/tricase_test_questions_generator.py
+++ QuantaTools/maths_tools/maths_test_questions/tricase_test_questions_generator.py
@@ -149,7 +149,7 @@ def make_tricase_questions(
     questions = []
     exceptions = []
     assert qtype in [None, QType.MATH_ADD, QType.MATH_SUB, QType.MATH_NEG, QType.UNKNOWN], f"Qtype must be none, sub, neg or unknown, but received {qtype}"
-    assert test_case in [8, 9, 10], f"Tricase test cases must be 8,9 or 10, received {test_case}"
+    assert isinstance(test_case, TriCaseBehavior), f"Tricase test cases must be of type TriCaseBehavior"
     assert operation in [MathsToken.PLUS, MathsToken.MINUS], f"Tricase operation must be in [plus,minus]={[MathsToken.PLUS, MathsToken.MINUS]}, received operation {operation}"
 
     attempts = 0
